19d2113115350a86c7944b94a1e70b5031764d3c
Dev Ttangkong <ttankkeo112@gmail.com>
2025-05-18 04:20:24 +0900

    Added RefreshIndicator widget

--- lib/flutter_refresh_indicator.dart
+++ lib/flutter_refresh_indicator.dart
@@ -1,3 +1,5 @@
 library;
 
 export 'widgets/clamping_refresh_indicator.dart';
+export 'widgets/bouncing_refresh_indicator.dart';
+export 'widgets/refresh_indicator.dart';
---
+++ lib/widgets/refresh_indicator.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_refresh_indicator/flutter_refresh_indicator.dart';
+
+enum RefreshIndicatorType {
+  clamping,
+  bouncing
+}
+
+class RefreshIndicator extends StatefulWidget {
+  const RefreshIndicator({
+    super.key,
+    this.type,
+    required this.onRefresh,
+    required this.child
+  });
+
+  final RefreshIndicatorType? type;
+  final AsyncCallback onRefresh;
+  final Widget child;
+
+  @override
+  State<RefreshIndicator> createState() => _RefreshIndicatorState();
+}
+
+class _RefreshIndicatorState extends State<RefreshIndicator> {
+  ScrollPhysics getScrollPhysics() {
+    return ScrollConfiguration.of(context).getScrollPhysics(context);
+  }
+
+  bool get isBouncing => getScrollPhysics() is BouncingScrollPhysics;
+
+  @override
+  Widget build(BuildContext context) {
+    final RefreshIndicatorType current = widget.type ?? (isBouncing
+      ? RefreshIndicatorType.bouncing
+      : RefreshIndicatorType.clamping
+    );
+    
+    if (current == RefreshIndicatorType.clamping) {
+      return BouncingRefreshIndicator(onRefresh: widget.onRefresh, child: widget.child);
+    } else {
+      return ClampingRefreshIndicator(onRefresh: widget.onRefresh, child: widget.child);
+    }
+  }
+}

77382d6ea8f87aa452e07cedc33af66230376834
Dev Ttangkong <ttankkeo112@gmail.com>
2025-05-19 02:42:30 +0900

    Addex simple example file

---
+++ example/main.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart' hide RefreshIndicator;
+import 'package:flutter_refresh_indicator/flutter_refresh_indicator.dart';
+
+void main() {
+  runApp(MainApp());
+} 
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: SafeArea(
+          child: RefreshIndicator(
+            onRefresh: () => Future.delayed(Duration(milliseconds: 1000)),
+            child: ListView.builder(
+              itemCount: 100,
+              itemBuilder: (context, index) {
+                return Container(
+                  color: index % 2 == 0 ? Colors.red : Colors.blue,
+                  height: 50,
+                );
+              },
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
